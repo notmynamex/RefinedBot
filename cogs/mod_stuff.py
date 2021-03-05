62ceccf8d94d3efa37f0b288c3c710b6448da88b
Ben <bcboos05@gmail.com>
2021-03-05 11:49:50 +0100

    Update mod_stuff.py

--- cogs/mod_stuff.py
+++ cogs/mod_stuff.py
@@ -13,5 +13,18 @@ class mod_stuff(commands.Cog):
             await ctx.channel.purge(limit=limit)
             print("the purge has begun")
 
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def ban (ctx, member:discord.User=None, reason =None):
+    if member == None or member == ctx.message.author:
+        await ctx.channel.send("why the fuck do you want to ban yourself??")
+        return
+    if reason == None:
+        reason = "begone thot"
+    message = f"begone {ctx.guild.name}, the thot banned for {reason}"
+    await member.send(message)
+    await ctx.guild.ban(member, reason=reason)
+    await ctx.channel.send(f"{member} is banned!")
+
 def setup(bot):
     bot.add_cog(mod_stuff(bot))
