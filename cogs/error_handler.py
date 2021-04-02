4b55e922f1e6db1ab070ba37c48311cd7f28bc29
notmynamex <59706340+notmynamex@users.noreply.github.com>
2021-04-02 21:27:03 +0200

    or maybe not

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -13,10 +13,6 @@ class error_handler(commands.Cog):
             print("some idiot thought there was a command but there wasnt lmao")
             return await ctx.send('I dont know that fucking command?? <:RinThink:807264385856700446>')
         
-        elif isinstance(error, discord.HTTPException):
-            print("httpexception, i hate those")
-            await ctx.send('cannot ban bots')
-        
         elif isinstance(error, commands.NSFWChannelRequired):
             print("some idiot tried posting nsfw content in a sfw channel")
             await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
