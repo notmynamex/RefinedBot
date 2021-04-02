e6bf2490f7c9b39faeb56c2c83a3f57c344cf2ad
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-04-02 15:27:15 +0100

    changed neko to use NSFWChannelRequired check

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -1,18 +1,28 @@
 import discord
 from discord.ext import commands
 
+
 class error_handler(commands.Cog):
     def __init__(self, client):
         self.client = client
+
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
             print("some idiot thought there was a command but there wasnt lmao")
             return await ctx.send('I dont know that fucking command?? <:RinThink:807264385856700446>')
+        
+        elif isinstance(error, commands.NSFWChannelRequired):
+            print("some idiot tried posting nsfw content in a sfw channel")
+            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+
         elif isinstance(error, commands.CheckFailure):
             print("some idiot thought they had perms to do mod stuff lmao")
             return await ctx.send('How about you get the fucking perms to do that first?')
+        
+        else:
+            print(error)
 
 def setup(bot):
     bot.add_cog(error_handler(bot))
--- cogs/neko.py
+++ cogs/neko.py
@@ -31,230 +31,168 @@ class neko(commands.Cog):
         await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/fox_girl"), "fox.png")) #also vital please believe me thanks
 
     @bot.group()
+    @commands.is_nsfw()
     async def feet(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/feet"), "feet.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/feet"), "feet.png"))
 
     @bot.command()
+    @commands.is_nsfw()
     async def yuri(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/yuri"), "yuri.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/yuri"), "yuri.png"))
 
     @bot.command()
+    @commands.is_nsfw()
     async def futa(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/futanari"), "futa.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/futanari"), "futa.png"))
 
     @bot.command()
+    @commands.is_nsfw()
     async def lewdkemo(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/lewdkemo"), "lewdkemo.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/lewdkemo"), "lewdkemo.png"))
 
     @feet.command(aliases=["gif"])
+    @commands.is_nsfw()
     async def feet_gif(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/feetg"), "feet.gif"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
 
     @bot.group(invoke_without_command=True)
+    @commands.is_nsfw()
     async def cum(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/cum"), "cum.gif"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/cum"), "cum.gif"))
 
     @bot.command()
+    @commands.is_nsfw()
     async def erokemo(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/erokemo"), "erokemo.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/erokemo"), "erokemo.png"))
 
     @bot.command()
+    @commands.is_nsfw()
     async def les(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/les"), "les.gif"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/les"), "les.gif"))
 
     @bot.command()
+    @commands.is_nsfw()
     async def lewdfox(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/lewdk"), "lewdk.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/lewdk"), "lewdk.png"))
 
     @bot.command()
+    @commands.is_nsfw()
     async def eroyuri(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/eroyuri"), "eroyuri.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/eroyuri"), "eroyuri.png"))
 
     @bot.command()
+    @commands.is_nsfw()
     async def eron(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/eron"), "eron.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/eron"), "eron.png"))
 
     @cum.command(aliases=["jpg"])
+    @commands.is_nsfw()
     async def cum_jpg(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/cum_jpg"), "cum.jpg"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/cum_jpg"), "cum.jpg"))
 
     @bot.command()
+    @commands.is_nsfw()
     async def bj(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/bj"), "bj.gif"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/bj"), "bj.gif"))
 
     @bot.command()
+    @commands.is_nsfw()
     async def solo(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/solo"), "solo.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/solo"), "solo.png"))
 
     @bot.command()
     async def kemo(self, ctx):
         await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/kemonomimi"), "kemonomimi.png"))
 
     @bot.command()
+    @commands.is_nsfw()
     async def nsfw_avatar(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/nsfw_avatar"), "nsfw_avatar.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/nsfw_avatar"), "nsfw_avatar.png"))
 
     @bot.command()
+    @commands.is_nsfw()
     async def anal(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/anal"), "anal.gif"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
 
     @bot.command()
+    @commands.is_nsfw()
     async def hentai(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/hentai"), "hentai.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
 
     @bot.command()
+    @commands.is_nsfw()
     async def erofeet(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/erofeet"), "erofeet.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
 
     @bot.command()
+    @commands.is_nsfw()
     async def keta(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/keta"), "keta.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
 
     @bot.command()
+    @commands.is_nsfw()
     async def blowjob(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/blowjob"), "blowjob.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
 
     @bot.group(invoke_without_command=True)
+    @commands.is_nsfw()
     async def pussy(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/pussy"), "pussy.gif"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
 
     @bot.command()
+    @commands.is_nsfw()
     async def tits(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/tits"), "tits.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
 
     @bot.command()
     async def lizard(self, ctx):
         await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/lizard"), "lizard.png"))
 
     @pussy.command(aliases=["jpg"])
+    @commands.is_nsfw()
     async def pussy_jpg(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/pussy_jpg"), "pussy.jpg"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
 
     @bot.command()
+    @commands.is_nsfw()
     async def pwankg(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/pwankg"), "pwankg.gif"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
 
     @bot.command()
+    @commands.is_nsfw()
     async def classic(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/classic"), "classic.gif"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
 
     @bot.command()
+    @commands.is_nsfw()
     async def kuni(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/kuni"), "kuni.gif"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
 
     @bot.command()
+    @commands.is_nsfw()
     async def femdom(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/femdom"), "femdom.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
 
     @bot.command()
+    @commands.is_nsfw()
     async def ero_kitsune(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/erok"), "erok.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')   
+            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/erok"), "erok.png")) 
 
     @bot.command()
+    @commands.is_nsfw()
     async def fox_girl(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/fox_girl"), "fox_girl.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/fox_girl"), "fox_girl.png"))
 
     @bot.command()
+    @commands.is_nsfw()
     async def boobs(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/boobs"), "boobs.gif"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/boobs"), "boobs.gif"))
 
     @bot.command()
+    @commands.is_nsfw()
     async def ero(self, ctx):
-        if ctx.channel.is_nsfw() is True:
-            await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/ero"), "ero.png"))
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
-
+        await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/ero"), "ero.png"))
+    
     @bot.command()
     async def smug(self, ctx):
         await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/smug"), "smug.gif"))
@@ -264,19 +202,14 @@ class neko(commands.Cog):
         await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/goose"), "goose.png"))
 
     @neko.group(invoke_without_command=True)
+    @commands.is_nsfw()
     async def lewd(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/lewd"), "neko_lewd.png")) #equally as vital
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
 
     @lewd.command(aliases=["gif"])
+    @commands.is_nsfw()
     async def lewd_gif(self, ctx):
-        if ctx.channel.is_nsfw() is True:
             await ctx.send(file=discord.File(await image("https://nekos.life/api/v2/img/nsfw_neko_gif"), "neko_lewd_gif.gif")) #this too
-        if ctx.channel.is_nsfw() is False:
-            await ctx.send('go to a nsfw channel you fucking idiot <:RinKEK:802258335062949888>')
-
 
 def setup(bot):
     bot.add_cog(neko(bot))
