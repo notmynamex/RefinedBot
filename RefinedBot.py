889ba42a16f3774c1aba6e8119016cc57feb2db5
notmyname <bcboos05@gmail.com>
2021-04-23 18:46:31 +0200

    moved status shit to a cog

--- RefinedBot.py
+++ RefinedBot.py
@@ -6,8 +6,6 @@ from discord.ext import tasks
 from random import randint
 from dotenv import load_dotenv
 from utils import jskp
-from random import getrandbits
-from random import choice
 
 
 load_dotenv(os.getcwd()+"/config.env")
@@ -20,44 +18,6 @@ client = discord.Client()
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
 
-status_list = [
-    "try --mention",
-    "fuck everyone who is reading this",
-    "you fucking suck",
-    "try --help",
-    "did you know you suck?",
-    "please dont crash me i am friendly bot yes",
-    "https://vndb.org/v4037",
-    "astolfo best waifu",
-    "pew pew you ded now haha",
-    "sirspam not gay",
-    "please use my commands please im kinda bored please use them"
-]
-
-watchlist = [
-    "HELO die",
-    "notmyname fail at life",
-    "notmyname look at hentai",
-    "notmyname fail at coding me",
-    "sirspam help notmyname",
-    "hentai",
-    "A certain scientific Railgun",
-    "A certain magical Index",
-    "a slime do politics"
-    "Dimension W",
-    "hayasaka be perfect as fuck",
-    "astolfo do astolfo things",
-    "dead people get sent into elevators",
-    "some guy teach magic to a carpet",
-    "konosuba season 1 because season 2 was bad",
-    "highschool dxd for the 64th time",
-    "not hololive",
-    "some scottish guy kill a lot of people",
-    "berserk 2016",
-    "kansen sodom"
-]
-
-
 initial_cogs = [
     "jishaku",
     "cogs.error_handler",
@@ -66,7 +26,8 @@ initial_cogs = [
     "cogs.gelb",
     "cogs.mod_stuff",
     "cogs.help",
-    "cogs.etc"
+    "cogs.etc",
+    "cogs.status"
 ]
 
 for cog in initial_cogs:
@@ -76,21 +37,8 @@ for cog in initial_cogs:
     except Exception as e:
         logging.error(f"Failed to load cog {cog}: {e}")
 
-@tasks.loop(minutes=10)
-async def status(self):
-    await self.bot.wait_until_ready()
-    if getrandbits(1) == 1:
-        value = choice(status_list)
-        await self.bot.change_presence(activity=discord.Game(name=value))
-        logging.info(f"Status set to: {value}")
-    else:
-        value = choice(watchlist)
-        await self.bot.change_presence(activity=discord.Activity(name=value, type=discord.ActivityType.watching))
-        logging.info(f"Status set to: {value}")
-
 @bot.event
-async def on_ready(self):
+async def on_ready():
     logging.info('Logged in as {0.user}'.format(bot))
-    self.status.start()
 
 bot.run(os.getenv("TOKEN"))
---
+++ cogs/status.py
@@ -0,0 +1,68 @@
+from random import getrandbits
+from random import choice
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+import logging
+
+status_list = [
+    "try --mention",
+    "fuck everyone who is reading this",
+    "you fucking suck",
+    "try --help",
+    "did you know you suck?",
+    "please dont crash me i am friendly bot yes",
+    "https://vndb.org/v4037",
+    "astolfo best waifu",
+    "pew pew you ded now haha",
+    "sirspam not gay",
+    "please use my commands please im kinda bored please use them"
+]
+
+watchlist = [
+    "HELO die",
+    "notmyname fail at life",
+    "notmyname look at hentai",
+    "notmyname fail at coding me",
+    "sirspam help notmyname",
+    "hentai",
+    "A certain scientific Railgun",
+    "A certain magical Index",
+    "a slime do politics"
+    "Dimension W",
+    "hayasaka be perfect as fuck",
+    "astolfo do astolfo things",
+    "dead people get sent into elevators",
+    "some guy teach magic to a carpet",
+    "konosuba season 1 because season 2 was bad",
+    "highschool dxd for the 64th time",
+    "not hololive",
+    "some scottish guy kill a lot of people",
+    "berserk 2016",
+    "kansen sodom"
+]
+
+class status(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @tasks.loop(minutes=5)
+    async def status(self):
+        await self.bot.wait_until_ready()
+        if getrandbits(1) == 1:
+            value = choice(status_list)
+            await self.bot.change_presence(activity=discord.Game(name=value))
+            logging.info(f"Status set to: playing {value}")
+        else:
+            value = choice(watchlist)
+            await self.bot.change_presence(activity=discord.Activity(name=value, type=discord.ActivityType.watching))
+            logging.info(f"Status set to: watching {value}")
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.status.start()
+
+
+def setup(bot):
+    bot.add_cog(status(bot))
